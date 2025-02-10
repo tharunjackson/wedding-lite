1a006c9b9f4cf8151031716d4dc18ccb398b5216
tharunjackson <122014845+tharunjackson@users.noreply.github.com>
2025-02-10 18:40:27 +0530

    Add files via upload

---
+++ js/magnific-popup-options.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var magnifPopup=function(){$('.popup-image').magnificPopup({type:'image',removalDelay:300,mainClass:'mfp-with-zoom',gallery:{enabled:!0},zoom:{enabled:!0,duration:300,easing:'ease-in-out',opener:function(openerElement){return openerElement.is('img')?openerElement:openerElement.find('img')}}})};magnifPopup()})
